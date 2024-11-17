c643c2a33cc9e0d063165fa07c77beb551807ac4
Satvik Singh <satviksingh204@gmail.com>
2024-11-17 18:42:10 +0530

    changing js

--- client/client.js
+++ client/client.js
@@ -1,109 +1,94 @@
-console.log("working");
 const socket = io();
 let roomUniqueId = null;
 let player1 = false;
 
 function createGame() {
     player1 = true;
-    socket.emit("createGame");
+    socket.emit('createGame');
 }
 
-function joinGame(){
-    roomUniqueId = document.getElementById("roomUniqueId").value;
-    socket.emit('joinGame',{roomUniqueId: roomUniqueId});
+function joinGame() {
+    roomUniqueId = document.getElementById('roomUniqueId').value;
+    socket.emit('joinGame', {roomUniqueId: roomUniqueId});
 }
-socket.on("newGame",(data) => {
+
+socket.on("newGame", (data) => {
     roomUniqueId = data.roomUniqueId;
-    document.getElementById("initial").style.display = "none";
-    document.getElementById("gamePlay").style.display = "block";
+    document.getElementById('initial').style.display = 'none';
+    document.getElementById('gamePlay').style.display = 'block';
     let copyButton = document.createElement('button');
-    copyButton.innerText = "Copy Code";
-    copyButton.style.display = "block";
-    copyButton.addEventListener('click',() => {
-        navigator.clipboard.writeText(roomUniqueId).then(function () {
-            console.log("Async: Copy to clipboard was successful.");
-            
-        },function (err) {
-            console.log("Async: Could not copy text: ",err);
-            
-        })
-    })
+    copyButton.style.display = 'block';
+    copyButton.classList.add('btn','btn-primary','py-2', 'my-2')
+    copyButton.innerText = 'Copy Code';
+    copyButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(roomUniqueId).then(function() {
+            console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+    });
+    document.getElementById('waitingArea').innerHTML = `Waiting for opponent, please share code ${roomUniqueId} to join`;
+    document.getElementById('waitingArea').appendChild(copyButton);
+});
 
-    document.getElementById("waitingArea").innerHTML= `Waiting for opponent!, Please share code ${roomUniqueId} to join.`;
-    document.getElementById("waitingArea").appendChild(copyButton);
-    
-})
-
-
-socket.on("playersConnected",() => {
-  document.getElementById('waitingArea').style.display = "none";
-  document.getElementById("gameArea").style.display = "block";
-  document.getElementById("initial").style.display = "none";
+socket.on("playersConnected", () => {
+    document.getElementById('initial').style.display = 'none';
+    document.getElementById('waitingArea').style.display = 'none';
+    document.getElementById('gameArea').style.display = 'flex';
 })
 
 socket.on("p1Choice",(data)=>{
-    if (!player1) {
+    if(!player1) {
         createOpponentChoiceButton(data);
-        
     }
-})
-
+});
 
 socket.on("p2Choice",(data)=>{
-    if (player1) {
+    if(player1) {
         createOpponentChoiceButton(data);
     }
-})
+});
 
-function sendChoice(choice) {
-    const choiceEvent= player1 ? "p1Choice":"p2Choice";
+socket.on("result",(data)=>{
+    let winnerText = '';
+    if(data.winner != 'd') {
+        if(data.winner == 'p1' && player1) {
+            winnerText = 'You win';
+        } else if(data.winner == 'p1') {
+            winnerText = 'You lose';
+        } else if(data.winner == 'p2' && !player1) {
+            winnerText = 'You win';
+        } else if(data.winner == 'p2') {
+            winnerText = 'You lose';
+        }
+    } else {
+        winnerText = `It's a draw`;
+    }
+    document.getElementById('opponentState').style.display = 'none';
+    document.getElementById('opponentButton').style.display = 'block';
+    document.getElementById('winnerArea').innerHTML = winnerText;
+});
+
+function sendChoice(rpsValue) {
+    const choiceEvent= player1 ? "p1Choice" : "p2Choice";
     socket.emit(choiceEvent,{
-        rpsValue: choice,
+        rpsValue: rpsValue,
         roomUniqueId: roomUniqueId
-    })
-    let pcButton = document.createElement('button');
-    pcButton.style.display = "block";
-    pcButton.innerHTML= choice;
-    document.getElementById("player1choice").innerHTML = "";
-    document.getElementById("player1choice").append(pcButton);
+    });
+    let playerChoiceButton = document.createElement('button');
+    playerChoiceButton.style.display = 'block';
+    playerChoiceButton.classList.add(rpsValue.toString().toLowerCase());
+    playerChoiceButton.innerText = rpsValue;
+    document.getElementById('player1Choice').innerHTML = "";
+    document.getElementById('player1Choice').appendChild(playerChoiceButton);
 }
 
 function createOpponentChoiceButton(data) {
-    document.getElementById("opponentState").innerHTML = "Opponent made a choice";
-    let opponentBtn = document.createElement('button');
-    opponentBtn.id = "OpponentBtn";
-    opponentBtn.style.display = "none";
-    opponentBtn.innerHTML = data.rpsval;
-    document.getElementById('gameArea').appendChild(opponentBtn)
+    document.getElementById('opponentState').innerHTML = "Opponent made a choice";
+    let opponentButton = document.createElement('button');
+    opponentButton.id = 'opponentButton';
+    opponentButton.classList.add(data.rpsValue.toString().toLowerCase());
+    opponentButton.style.display = 'none';
+    opponentButton.innerText = data.rpsValue;
+    document.getElementById('player2Choice').appendChild(opponentButton);
 }
-
-socket.on("result",(data)=>{
-    let winnertext = "";
-    console.log("worked");
-    
-    if (data.winner == "d") {
-        winnertext = "Its a draw!";
-    }
-    else if (data.winner == "p2") {
-        if (!player1) {
-            winnertext = "You Won!";
-        }else{
-            winnertext = "You Lose!";
-        }
-    }else{
-        if (player1) {
-            winnertext = "You Won!";
-        }else{
-            winnertext = "You Lose!";
-        }  
-        
-    }
-
-    document.getElementById('opponentState').style.display = "none";
-    document.getElementById('OpponentBtn').style.display = "block";
-    let winnertxt =  document.createElement('p');
-    winnertxt.innerHTML = winnertext;;
-    document.getElementById("winnerArea").appendChild(winnertxt);
-    document.getElementById('winnerArea').style.display= "block";
-
-})
